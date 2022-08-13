e74340532c4a59fba8ab14e59944c6d82e82fd0f
Viet Hung Nguyen <vhn@sfu.ca>
2022-08-12 22:25:45 -0700

    Add keyboard handlers

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,14 +1,24 @@
 import { Component, For } from "solid-js";
 import { KEYBOARD_ROWS } from "../constants";
+import game from "../utils/game";
 
 const Keyboard: Component = () => {
+  const {addLetter, removeLetter, submitWord} = game;
   return <div>
     <For each={KEYBOARD_ROWS}>
       {(row, i) => <div class="flex gap-2 justify-center mb-2">
         <For each={row}>
-          {(key, i) => <button class="p-2 h-10 rounded-md flex items-center justify-center border-2 border-gray-100">
-            {key}
-          </button>}
+          {(key, i) => (
+            <button
+              class="p-2 h-10 rounded-md flex items-center justify-center border-2 border-gray-100"
+              onClick={() => {
+                if (key === "ENTER") submitWord();
+                else if (key === "DEL") removeLetter();
+                else addLetter(key);
+              }}>
+              {key}
+            </button>
+          )}
         </For>
       </div>}
     </For>
